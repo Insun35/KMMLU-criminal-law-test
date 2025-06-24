abc487a35c0b2edf477a68a5354f2b8a593a63a9
Insun35 <inseonyu7@gmail.com>
2025-06-24 16:10:07 +0900

    chore: rename prepare_data.py to load_data.py

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -21,16 +21,15 @@ def save_to_json(data, out_file: Path):
 def load_kmmlu_test(save: bool = True):
     print("🔄 Loading test set ...")
     test_set = load_dataset("HAERAE-HUB/KMMLU", "Criminal-Law", split="test")
-    print(f"✅ Successfully loaded {len(test_set)} questions")
     if save:
         save_to_json(test_set.to_list(), KMMLU_OUT_FILE)
+        print(f"✅ Successfully saved {len(test_set)} questions")
     return test_set
 
 
 def load_ljp_criminal_data(save: bool = True):
     print("🔄 Loading ljp criminal data ...")
     ljp_criminal_data = load_dataset("lbox/lbox_open", "ljp_criminal", split="train")
-    print(f"✅ Successfully loaded {len(ljp_criminal_data)} data samples")
     if save:
         with LBOX_OUT_FILE.open("w", encoding="utf-8") as f:
             for case in tqdm(ljp_criminal_data, desc="Saving to JSONL"):
@@ -40,6 +39,7 @@ def load_ljp_criminal_data(save: bool = True):
                     {"id": case["id"], "text": text}, ensure_ascii=False
                 )
                 f.write(json_line + "\n")
+        print(f"✅ Successfully saved {len(ljp_criminal_data)} data samples")
     return ljp_criminal_data
 
 
