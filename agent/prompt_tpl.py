109b413dab43a3cb96300530b847f544cc0b500d
Insun35 <inseonyu7@gmail.com>
2025-06-25 20:31:34 +0900

    feat: implement retriever module

--- agent/prompt_tpl.py
+++ agent/prompt_tpl.py
@@ -1,4 +1,4 @@
-SIMPLE_PROMPT = """
+PROMPT_TPL = """
 다음은 4지선다 형법(Criminal Law) 문제이다. 정답은 1, 2, 3, 4 중 하나만, 한 글자로 대답하라.
 
 질문: {question}
@@ -8,6 +8,9 @@ SIMPLE_PROMPT = """
 2. {B}
 3. {C}
 4. {D}
+
+# 검색된 법률 근거:
+{context}
 
 정답:
 """
